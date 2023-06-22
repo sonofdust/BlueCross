806b03611cf1309a0fd572c47dcaea77ef4a9a40
sonofdust <sonofdust@gmail.com>
2023-06-21 23:13:52 -0600

    Cleanup

--- auth.js
+++ auth.js
@@ -30,94 +30,11 @@ app.post("/login", (req, res) => {
   security.LogIn(req, res);
 });
 
-// // User login endpoint
-// app.post("/login", (req, res) => {
-//   const {email, password} = req.body;
-
-//   // Find user by username
-//   const user = users.find((u) => u.email === email);
-//   if (!user) {
-//     return res.status(401).json({error: "Invalid email or password"});
-//   }
-//   //*****************************************************************************
-//   //   bcrypt.hash(password, 10, (err, hash) => {
-//   //     if (err) {
-//   //       console.error(err);
-//   //       return;
-//   //     }
-//   //     console.log("Hashed password:", hash);
-//   //   });
-//   //*****************************************************************************
-//   // Compare password
-//   bcrypt.compare(password, user.password, (err, result) => {
-//     console.log(`RESULT:  ${result} ERROR: ${err}`);
-//     if (err || !result) {
-//       return res.status(401).json({error: "BCRYPT Invalid email or password"});
-//     }
-//     // Generate JWT token
-//     const token = jwt.sign({userId: user.id}, SECRET_KEY, {expiresIn: "1h"});
-//     res.json({token});
-//   });
-// });
-
-// app.get("/protected", (req, res) => {
-//   // Verify JWT token
-
-//   const token = req.headers.authorization?.split(" ")[1];
-//   if (!token) {
-//     return res.status(401).json({error: "Authorization token missing"});
-//   }
-
-//   jwt.verify(token, SECRET_KEY, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).json({error: "Invalid token"});
-//     }
-
-//     // Token is valid
-//     res.json({message: "Protected route accessed successfully", user: decoded});
-//   });
-// });
-
-// const callUsersAPI = () => {
-//   return new Promise((resolve, reject) => {
-//     const options = {
-//       hostname: "reqres.in",
-//       path: "/api/users?page=2",
-//       method: "GET",
-//     };
-
-//     const req = https.request(options, (res) => {
-//       let data = "";
-
-//       res.on("data", (chunk) => {
-//         data += chunk;
-//       });
-
-//       res.on("end", () => {
-//         resolve(JSON.parse(data));
-//       });
-//     });
-
-//     req.on("error", (error) => {
-//       reject(error);
-//     });
-
-//     req.end();
-//   });
-// };
-
 app.get("/users", async (req, res) => {
   // Verify JWT token
   await security.Validate(req, res);
   //****************************************************************************************** */
   external.getUsers(res);
-  //   this.callUsersAPI()
-  //     .then((data) => {
-  //       res.json(data);
-  //     })
-  //     .catch((error) => {
-  //       res.err(error);
-  //     });
   //****************************************************************************************** */
 });
 
